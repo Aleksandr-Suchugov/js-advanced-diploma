125a411c28f4423c3f0096be98fa202a226005e0
aleksandr-suchugov <alex.coup@yandex.ru>
2022-06-10 17:48:43 +0300

    more fixes

--- src/js/themes.js
+++ src/js/themes.js
@@ -1,23 +1,8 @@
-export default class Themes {
-  static level = 0;
+const themes = {
+  prairie: 'prairie',
+  desert: 'desert',
+  arctic: 'arctic',
+  mountain: 'mountain',
+};
 
-  static _themes = [
-    { prairie: 'prairie' },
-    { desert: 'desert' },
-    { arctic: 'arctic' },
-    { mountain: 'mountain' },
-  ];
-
-  static get themes() {
-    return this._themes;
-  }
-
-  static nextLevel() {
-    const themeObj = this._themes[this.level];
-    this.level += 1;
-    if (this._themes.length === this.level) {
-      this.level = 0;
-    }
-    return Object.values(themeObj);
-  }
-}
+export default themes;
